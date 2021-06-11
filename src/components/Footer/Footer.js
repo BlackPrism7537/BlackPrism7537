f26665ed4a12956d25f5e0c775aa4df13b21d66d
ZachBarcoe <z.barcoe@gmail.com>
2021-06-11 22:12:03 +0000

    remove links from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,15 +34,6 @@ const Footer = () => {
 				<CompanyContainer>
 					<Slogan>Red. Green. Refactor.</Slogan>
 				</CompanyContainer>
-				<SocialIcons href="https://github.com/BlackPrism7537">
-					<AiFillGithub size="3rem" />
-				</SocialIcons>
-				<SocialIcons href="https://twitter.com/Z_Barcoe">
-					<AiFillTwitterCircle size="3rem" />
-				</SocialIcons>
-				<SocialIcons href="https://www.instagram.com/z.barcoe/">
-					<AiFillInstagram size="3rem" />
-				</SocialIcons>
 			</SocialIconsContainer>
 		</FooterWrapper>
 	);
